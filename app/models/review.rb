ddcbcfd6a0e132ba7d5fe07b37c814ea1fb7f81b
Code 3 <cod3@Codes-MBP.lan1>
2020-09-15 10:08:29 -0700

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
     belongs_to :user
-    validates :title, :image_url, :description, presence: true
+    validates :title, :image_url, :description, presence: true, uniqueness: true
+
+
 end
