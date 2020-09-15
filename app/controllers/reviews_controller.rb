d7d4cf507c6e354176e24be000ba683d63644b4c
Code 3 <cod3@Codes-MacBook-Pro.local>
2020-09-14 23:41:45 -0700

    rev

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,7 @@ class ReviewsController < ApplicationController
 
   get "/reviews/new" do
       if logged_in?
+        #@review = Review.new
           erb :"reviews/new"
       else
           flash[:error] = "User must log in"
@@ -20,8 +21,8 @@ class ReviewsController < ApplicationController
           flash[:message] = "New review" 
           redirect "/reviews/#{@review.id}"
       else
-          flash[:error] = "review failed #{@review.errors.full_messages.to_sentence}"
-      redirect "/reviews/new"
+          flash[:error] = "review failed "
+          redirect "/reviews"
       end
   end
 
@@ -31,27 +32,27 @@ class ReviewsController < ApplicationController
       erb :"/reviews/show"
   end
 
-  get 'reviews/:id/edit' do
-      @review = Review.find(params[:id])
-      if authorized_to_edit?(@review)
-          erb :'/reviews/edit'
-      else
-          flash[:error] = "Not authorized to edit that review!"
-          redirect "/reviews"
-      end
+  get '/reviews/:id/edit' do
+    set_review
+    redirect_if_not_authorized
+    erb :"reviews/edit"
   end
 
   patch '/reviews/:id' do 
       #@review = Review.find(params[:id])
-      @review.update(title: params[:title], image_url: params[:image_url], description: params[:description])
-      redirect "/reviews/#{@review.id}"
-  end
-  
-
-  delete '/reviews/:id' do
-     #@review = Review.find(params[:id])
       set_review
       redirect_if_not_authorized
+      if @review.update(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+       flash[:success] = "Post successfully updated"
+       redirect "/reviews/#{@review.id}"
+     else
+       erb :"/reviews/edit"
+     end
+  end
+
+  delete '/reviews/:id' do
+    redirect_if_not_logged_in
+      @review = Review.find(params[:id])
       @review.destroy
       redirect '/reviews'
   end
@@ -61,9 +62,23 @@ private
   def set_review
     @review = Review.find_by_id(params[:id])
       if @review.nil?
-       flash[:error] = "Couldn't find a Post with id: #{params[:id]}"
+       flash[:error] = "Couldn't find Review"
        redirect "/reviews"
       end
+  end
+
+ def authorized_to_edit?(review)
+    @review.user == current_user
+  end
+
+
+  def redirect_if_not_authorized
+    redirect_if_not_logged_in
+    if !authorized_to_edit?(@review)
+      flash[:error] = "You don't have permission to do that action"
+      redirect "/reviews"
+    end
+
   end
 
 end
