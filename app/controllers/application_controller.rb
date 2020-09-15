35e79cbdc1754dd0d4a627abf83ea9f9e299a575
Code 3 <cod3@Codes-MacBook-Pro.local>
2020-09-14 23:41:34 -0700

    app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,28 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     #then set a session secret for extra layer of security
     set :session_secret, "dbz"
+    set :method_override, true
     register Sinatra::Flash
   end
 
   get "/" do
     if logged_in?
-      redirect "/user/#{current_user.id}"
+      redirect "/users/#{current_user.id}"
     else
       erb :welcome
     end
   end
 
-  helpers do
+ # not_found do
+ #   flash[:error] = "Not found"
+  #  if logged_in?
+   #   redirect :'/reviews'
+   # else
+    #  redirect '/'
+   # end
+  #end
+
+  private
 
     def logged_in?
       !!current_user
@@ -34,18 +44,14 @@ class ApplicationController < Sinatra::Base
                                          #User.find(session[user_id]) would work also but we need a truthy or a falsey value
     end
 
-    def authorized_to_edit?(review)
-      @review.user == current_user
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:error] = "You must be logged in to view that page"
+        redirect request.referrer || "/login"
+      end
     end
-
-  end
-
-  private
+    
+  
  
-  def redirect_if_not_logged_in
-    if !logged_in?
-      flash[:error] = "You must be logged in to view that page"
-      redirect request.referrer || "/login"
-    end
-  end
+ 
 end
